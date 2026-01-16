2e1c64bc20da472e00a80447d7aff5400dab4050
Rece Graham <graharec@oregonstate.edu>
2026-01-15 16:19:05 -0800

    Completed the assignment

--- prog1.c
+++ prog1.c
@@ -1,14 +1,11 @@
 #include <stdio.h> // Necessary for printf()
 #include <time.h>
 #include <stdlib.h>
-// TODO Other includes
 
-
-// Generate random numbers
+// assign1
+// does all the stuff this assignment is supposed to do
 
 void assign1() {
-	//gongaga
-	//
 	int myrand = rand() % 11;
 
 	if ((myrand >= 0) && (myrand <= 4)){
@@ -17,16 +14,15 @@ void assign1() {
 	else if ((myrand >= 5) && (myrand <= 9)){
 		printf("FRODO LIVES!\n");
 	}
-
 	else {
 		printf("Larn is the best roguelike\n");
 	}
-
 	printf("The random number was: %d\n", myrand);
-
 }
+
 
 int main() {
 	srand(time(NULL));
 	assign1();
 }
+
