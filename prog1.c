d88bd0dfbff805919eb12d38a3ec9b01ae91a8e5
Rece Graham <graharec@oregonstate.edu>
2026-01-15 16:10:35 -0800

    completed the assignment

--- prog1.c
+++ prog1.c
@@ -1,11 +1,32 @@
 #include <stdio.h> // Necessary for printf()
-
+#include <time.h>
+#include <stdlib.h>
 // TODO Other includes
 
 
+// Generate random numbers
+
+void assign1() {
+	//gongaga
+	//
+	int myrand = rand() % 11;
+
+	if ((myrand >= 0) && (myrand <= 4)){
+		printf("Eat more beef, kick less cats\n");
+	}
+	else if ((myrand >= 5) && (myrand <= 9)){
+		printf("FRODO LIVES!\n");
+	}
+
+	else {
+		printf("Larn is the best roguelike\n");
+	}
+
+	printf("The random number was: %d\n", myrand);
+
+}
+
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	srand(time(NULL));
+	assign1();
 }
